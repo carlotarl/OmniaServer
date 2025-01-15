9db7f482915f2dbf06a7613e917490855e28ba96
carlotarl <84837470+carlotarl@users.noreply.github.com>
2025-01-15 16:48:07 +0100

    Update database.js

--- database.js
+++ database.js
@@ -9,7 +9,8 @@ const conexion = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    port: process.env.PORT
 })
 
 // Hacemos la estructura try catch para poder parar los errores
